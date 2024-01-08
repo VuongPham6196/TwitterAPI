a36debd0b20f2e2c4683b8a1cb9b623a5079f64c
vuongphamdev <phamnhuvuong96@gmail.com>
2024-01-08 22:22:42 +0700

    Fix error

--- src/controllers/medias.controller.ts
+++ src/controllers/medias.controller.ts
@@ -1,11 +1,5 @@
 import { NextFunction, Request, Response } from 'express'
-import formidable, { errors as formidableErrors } from 'formidable'
-import { isEmpty } from 'lodash'
-import path from 'path'
-import { UPLOAD_IMAGE_TEMP_DIR } from '~/constants/dir'
-import HTTP_STATUS from '~/constants/httpStatus'
 import { UPLOAD_MESSAGE } from '~/constants/messages'
-import { ErrorWithStatus } from '~/models/schemas/Errors'
 import mediasServices from '~/services/medias.services'
 
 export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
